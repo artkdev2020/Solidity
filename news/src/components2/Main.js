e095fcf496c80d367138d26a5caed24bd9a957a1
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-08-16 09:51:27 +0300

    made the input form a separate component

--- news/src/components2/Main.js
+++ news/src/components2/Main.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Input from "./Input";
 import Post from "./Post";
 
 const Main = props => {
@@ -15,29 +15,11 @@ const Main = props => {
         >
           <div className="content mr-auto ml-auto">
             <p>&nbsp;</p>
-            <form
-              onSubmit={event => {
-                event.preventDefault();
-                const content = postContent.value;
-                props.createPost(content);
-              }}
-            >
-              <div className="form-group mr-sm-2">
-                <input
-                  id="postContent"
-                  type="text"
-                  ref={input => {
-                    setPostContent(input);
-                  }}
-                  className="form-control"
-                  placeholder="What's on your mind?"
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-primary btn-block">
-                Share
-              </button>
-            </form>
+            <Input
+              postContent={postContent}
+              setPostContent={setPostContent}
+              createPost={props.createPost}
+            />
             <p>&nbsp;</p>
             {props.posts.map((post, key) => (
               <Post post={post} postKey={key} tipPost={props.tipPost} />
