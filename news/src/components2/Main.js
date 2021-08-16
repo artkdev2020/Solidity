c57d2fa9e08f252c329316642f106e0844fca596
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-08-16 09:44:07 +0300

    made the post as a separate component

--- news/src/components2/Main.js
+++ news/src/components2/Main.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import Identicon from "identicon.js";
+
+import Post from "./Post";
 
 const Main = props => {
   const [postContent, setPostContent] = useState(null);
@@ -38,53 +39,9 @@ const Main = props => {
               </button>
             </form>
             <p>&nbsp;</p>
-            {props.posts.map((post, key) => {
-              return (
-                <div className="card mb-4" key={key}>
-                  <div className="card-header">
-                    <img
-                      className="mr-2"
-                      width="30"
-                      height="30"
-                      alt="icon"
-                      src={`data:image/png;base64,${new Identicon(
-                        post.author,
-                        30
-                      ).toString()}`}
-                    />
-                    <small className="text-muted">{post.author}</small>
-                  </div>
-                  <ul id="postList" className="list-group list-group-flush">
-                    <li className="list-group-item">
-                      <p>{post.content}</p>
-                    </li>
-                    <li key={key} className="list-group-item py-2">
-                      <small className="float-left mt-1 text-muted">
-                        TIPS:{" "}
-                        {window.web3.utils.fromWei(
-                          post.tipAmount.toString(),
-                          "Ether"
-                        )}{" "}
-                        ETH
-                      </small>
-                      <button
-                        className="btn btn-link btn-sm float-right pt-0"
-                        name={post.id}
-                        onClick={event => {
-                          let tipAmount = window.web3.utils.toWei(
-                            "0.1",
-                            "Ether"
-                          );
-                          props.tipPost(event.target.name, tipAmount);
-                        }}
-                      >
-                        TIP 0.1 ETH
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-              );
-            })}
+            {props.posts.map((post, key) => (
+              <Post post={post} postKey={key} tipPost={props.tipPost} />
+            ))}
           </div>
         </main>
       </div>
