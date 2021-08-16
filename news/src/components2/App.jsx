c52e3427745b10d7c12a92d040d77fa8daef5c87
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-08-16 13:26:46 +0300

    remove unnecessary comments

--- news/src/components2/App.jsx
+++ news/src/components2/App.jsx
@@ -13,20 +13,6 @@ const App = () => {
 
   useEffect(() => {
     Web3Api.loadWeb3();
-
-    // const loadWeb3 = async () => {
-    //   if (window.ethereum) {
-    //     window.web3 = new Web3(window.ethereum);
-    //     await window.ethereum.enable();
-    //   } else if (window.web3) {
-    //     window.web3 = new Web3(window.web3.currentProvider);
-    //   } else {
-    //     window.alert(
-    //       "Non-Ethereum browser detected. You should consider trying MetaMask!"
-    //     );
-    //   }
-    // };
-    // loadWeb3();
   }, []);
 
   useEffect(() => {
@@ -36,33 +22,6 @@ const App = () => {
       setPostCount,
       setPosts
     );
-    // const loadBlockchainData = async () => {
-    //   const web3 = window.web3;
-    //   const accounts = await web3.eth.getAccounts();
-    //   setAccount(accounts[0]);
-    //   const networkId = await web3.eth.net.getId();
-    //   const networkData = News.networks[networkId];
-    //   if (networkData) {
-    //     const newsNetworkLocal = new web3.eth.Contract(
-    //       News.abi,
-    //       networkData.address
-    //     );
-    //     setNewsNetwork(newsNetworkLocal);
-    //     const postCount = await newsNetworkLocal.methods.postCount().call();
-    //     setPostCount(postCount);
-    //     let tmpPosts = [];
-    //     for (let i = 0; i <= postCount; i++) {
-    //       const post = await newsNetworkLocal.methods.posts(i).call();
-    //       if (post[3] != 0) {
-    //         tmpPosts.push(post);
-    //       }
-    //     }
-    //     setPosts(tmpPosts.sort((a, b) => b.tipAmount - a.tipAmount));
-    //   } else {
-    //     window.alert("NewsNetwork contract not deployed to detected network.");
-    //   }
-    // };
-    // loadBlockchainData();
   }, []);
 
   const createPost = content => {
