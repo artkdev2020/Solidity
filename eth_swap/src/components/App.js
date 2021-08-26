a54031338b11dd072820b6de9d5167376714552d
kokhnenko_vladimir <vladimir.kokhnenko@gmail.com>
2021-08-26 09:47:14 +0300

    modified app class component in app function component

--- eth_swap/src/components/App.js
+++ eth_swap/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Web3 from "web3";
 import Navbar from "./Navbar";
@@ -6,144 +6,125 @@ import EthSwap from "../abis/EthSwap.json";
 import Token from "../abis/Token.json";
 import Main from "./Main";
 
-class App extends Component {
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
+const App = props => {
+  const [account, setAccount] = useState();
+  const [ethBalance, setEthBalance] = useState();
+  const [token, setToken] = useState();
+  const [tokenBalance, setTokenBalance] = useState();
+  const [ethSwap, setEthSwap] = useState();
+  const [loading, setLoading] = useState(true);
+  const [content, setContent] = useState();
 
-  async loadBlockchainData() {
-    const web3 = window.web3;
-
-    const accounts = await web3.eth.getAccounts();
-    this.setState({ account: accounts[0] });
-
-    const ethBalance = await web3.eth.getBalance(this.state.account);
-    this.setState({ ethBalance });
-
-    const networkId = await web3.eth.net.getId();
-    const tokenData = Token.networks[networkId];
-
-    if (tokenData) {
-      const token = new web3.eth.Contract(Token.abi, tokenData.address);
-      this.setState({ token });
-      let tokenBalance = await token.methods
-        .balanceOf(this.state.account)
-        .call();
-      this.setState({ tokenBalance: tokenBalance.toString() });
-    } else {
-      window.alert("Token contract not deployed network data");
-    }
-
-    const ethSwapData = EthSwap.networks[networkId];
-    if (ethSwapData) {
-      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
-      this.setState({ ethSwap });
-    } else {
-      window.alert("EthSwap contract not deployed to detected network.");
-    }
-
-    this.setState({ loading: false });
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum);
-      await window.ethereum.enable();
-    } else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider);
-    } else {
-      window.alert(
-        "Non ethereum browser detected. You should consider trying Metavask!"
-      );
-    }
-  }
-
-  // setEtherBalanceInState = async etherAmount => {
-  //   const ethBalance = await window.web3.eth.getBalance(this.state.account);
-  //   const result = ethBalance - etherAmount;
-  //   this.setState({ ethBalance: result.toString() });
-  // };
-
-  // setDAppBalance = async tokenAmount => {
-  //   console.log(tokenAmount);
-  // };
-
-  buyTokens = etherAmount => {
-    this.setState({ loading: true });
-    this.state.ethSwap.methods
-      .buyToken()
-      .send({ value: etherAmount, from: this.state.account })
-      .on("transactionHash", hash => {
-        this.setState({ loading: false });
-        // this.setBalanceInState(etherAmount);
-      });
-  };
-
-  sellTokens = tokenAmount => {
-    console.log(tokenAmount);
-    this.setState({ loading: true });
-    this.state.token.methods
-      .approve(this.state.ethSwap.address, tokenAmount)
-      .send({ from: this.state.account })
-      .on("transactionHash", hash => {
-        this.state.ethSwap.methods
-          .sellToken(tokenAmount)
-          .send({ from: this.state.account })
-          .on("transactionHash", hash => {
-            this.setState({ loading: false });
-          });
-      });
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      account: "",
-      token: {},
-      ethSwap: {},
-      ethBalance: "0",
-      tokenBalance: "0",
-      loading: true
+  useEffect(() => {
+    const loadWeb3 = async () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      } else {
+        window.alert(
+          "Non ethereum browser detected. You should consider trying Metavask!"
+        );
+      }
     };
-  }
+    loadWeb3();
+  }, []);
 
-  render() {
-    let content;
-    if (this.state.loading) {
-      content = (
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const web3 = window.web3;
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const ethBalance = await web3.eth.getBalance(accounts[0]);
+      setEthBalance(ethBalance);
+
+      const networkId = await web3.eth.net.getId();
+      const tokenData = Token.networks[networkId];
+
+      if (tokenData) {
+        const token = new web3.eth.Contract(Token.abi, tokenData.address);
+        setToken(token);
+        let tokenBalance = await token.methods.balanceOf(accounts[0]).call();
+        setTokenBalance(tokenBalance.toString());
+      } else {
+        window.alert("Token contract not deployed network data");
+      }
+
+      const ethSwapData = EthSwap.networks[networkId];
+      if (ethSwapData) {
+        const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+        setEthSwap(ethSwap);
+      } else {
+        window.alert("EthSwap contract not deployed to detected network.");
+      }
+
+      setLoading(false);
+    };
+
+    loadBlockchainData();
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      setContent(
         <p id="loader" className="text-center">
           Loading...
         </p>
       );
     } else {
-      content = (
+      setContent(
         <Main
-          ethBalance={this.state.ethBalance}
-          tokenBalance={this.state.tokenBalance}
-          buyTokens={this.buyTokens}
-          sellTokens={this.sellTokens}
+          ethBalance={ethBalance}
+          tokenBalance={tokenBalance}
+          buyTokens={buyTokens}
+          sellTokens={sellTokens}
         />
       );
     }
+  }, [loading]);
 
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main
-              role="main"
-              className="col-lg-12 ml-auto mr-auto"
-              style={{ maxWidth: "600px" }}
-            >
-              <div className="content mr-auto ml-auto">{content}</div>
-            </main>
-          </div>
+  const buyTokens = etherAmount => {
+    setLoading(true);
+    ethSwap.methods
+      .buyToken()
+      .send({ value: etherAmount, from: account })
+      .on("transactionHash", hash => {
+        setLoading(false);
+      });
+  };
+
+  const sellTokens = tokenAmount => {
+    setLoading(true);
+    token.methods
+      .approve(ethSwap.address, tokenAmount)
+      .send({ from: account })
+      .on("transactionHash", hash => {
+        ethSwap.methods
+          .sellToken(tokenAmount)
+          .send({ from: account })
+          .on("transactionHash", hash => {
+            setLoading(false);
+          });
+      });
+  };
+
+  return (
+    <div>
+      <Navbar account={account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: "600px" }}
+          >
+            <div className="content mr-auto ml-auto">{content}</div>
+          </main>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
