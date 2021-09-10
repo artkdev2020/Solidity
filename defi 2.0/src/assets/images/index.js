d03e54434bfcc8588518f8f1cc1ad45c8a10c345
Kate Iskra <kateiskra99@gmail.com>
2021-09-10 14:59:31 +0300

    git rename

---
+++ defi 2.0/src/assets/images/index.js
@@ -0,0 +1,7 @@
+import dai from './dai.png'
+import ethLogo from './eth-logo.png'
+import farmer from './farmer.png'
+import logo from './logo.png'
+import tokenLogo from './token-logo.png'
+
+export { dai, ethLogo, farmer, logo, tokenLogo, }
